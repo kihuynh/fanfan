baef9dd8913e7e70992891d5888699d2f469f02d
Guest User <Guest@Epicodus-3C.local>
2018-04-19 15:52:54 -0700

    add create func and private book params

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,4 +10,17 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to books_path
+    else
+      render :new
+    end
+  end
+
+private
+  def book_params
+    params.require(:book).permit(:title)
+  end
