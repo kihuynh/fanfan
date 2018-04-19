4f7b3d988989ddef174ab8f0c31d97f3e0e8f149
Guest User <Guest@Epicodus-3C.local>
2018-04-19 10:17:44 -0700

    redo migrate

--- db/migrate/20180419171624_create_books.rb
+++ db/migrate/20180419171624_create_books.rb
@@ -2,6 +2,9 @@ class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
       t.column :title, :string
+      t.column :author, :string
+      t.column :publisher, :string
+      t.column :genre, :string
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_19_164001) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
