66a977cd8361b0be09a6f74484ba0eb9215bdf7e
Guest User <Guest@Epicodus-3C.local>
2018-04-19 14:18:53 -0700

    add rest of faker info. WE BE OUT HERE FAKING

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
 Book.destroy_all
 
 10.times do |index|
-  Book.create!(title: Faker::Book.title)
+  Book.create!(title: Faker::Book.title,
+               author: Faker::Book.author,
+               publisher: Faker::Book.publisher,
+               genre: Faker::Book.genre)
 end
 
 p "Created #{Book.count} books"
