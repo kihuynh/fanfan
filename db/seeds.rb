03590a805cbced7f2d01c9df607ada8b44a654be
Guest User <Guest@Epicodus-3C.local>
2018-04-19 11:03:13 -0700

    seed successful. time to cry, yeah?

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 book_list = [
   ["Harry Potter", "JK Rowling", "Some Pub", "fantasy"],
   ["Learn how to hate", "Hateful Larry", "Hate Industries", "self-help"],
-  ["How not to lose your mind", "JK Nope", "Done"]
+  ["How not to lose your mind", "JK Nope", "Done", "self-help"]
 ]
 
 book_list.each do |title, author, publisher, genre|
-  Book.create ( title_name: title, author_name: author, publisher_name: publisher, genre_name: genre)
+  Book.create(title: title, author: author, publisher: publisher, genre: genre)
 end
